5e830eed4c533664d7872c6f3074fafee285bd52
Mitch Garnatz <garn1876@stthomas.edu>
2023-07-06 18:23:42 -0500

    Assignment 1 Complete

--- client_test.py
+++ client_test.py
@@ -1,5 +1,6 @@
 import unittest
 from client3 import getDataPoint
+from client3 import getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,6 +9,16 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    test_price1 = (121.2 + 120.48) / 2
+    test_price2 = (121.68 + 117.87) / 2
+
+    prices = {}
+    for quote in quotes:
+      stock, bid_price, ask_price, price = getDataPoint(quote)
+      prices[stock] = price
+
+    self.assertEqual(prices["ABC"], test_price1, '\n\n*** Incorrect stock price' + ' ***\n')
+    self.assertEqual(prices["DEF"], test_price2, '\n\n*** Incorrect stock price' + ' ***\n')
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,11 +26,13 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    differences = {}
+    for quote in quotes:
+      stock, bid_price, ask_price, price = getDataPoint(quote)
+      differences[stock] = bid_price - ask_price
 
-
-  """ ------------ Add more unit tests ------------ """
-
-
+    self.assertGreater(differences["ABC"], 0, '\n\n*** Bid not greater than ask' + ' ***\n')
+    self.assertGreater(differences["DEF"], 0, '\n\n*** Bid not greater than ask' + ' ***\n')
 
 if __name__ == '__main__':
     unittest.main()
